f297b32f3bc548479c1c5314a64d6522b5492adc
Dev Vishnu <blazeiscoding24@gmail.com>
2025-07-04 01:14:29 +0530

    Updated app.py to use waitress for deployment

--- app.py
+++ app.py
@@ -24,4 +24,6 @@ def analyze():
         return "Invalid tool selected."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+
